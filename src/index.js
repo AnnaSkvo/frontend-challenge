4f483ccd846362fc9e80c5a96c82ba957c9ec484
Anna Skvortsova <annascvo@gmail.com>
2021-04-11 16:00:11 +0300

    corrections made

--- src/index.js
+++ src/index.js
@@ -3,21 +3,35 @@ new Vue({
   template: "#main",
   data: {
     index: true,
-    api: "https://api.thecatapi.com/v1/images/search?mime_types=jpg&&limit=100",
+    api: "https://api.thecatapi.com/v1/images/search?format=json&limit=100",
     allCats: [],
-    likeCats: []
+    likeCats: [],
+    hoverCat: null
   },
   methods: {
-    like: function (img) {
-      if (this.likeCats.includes(img)) {
-        return;
+    toggle: function (cat) {
+      if (this.likeCats.includes(cat)) {
+        this.disLike(cat);
+      } else {
+        this.like(cat);
       }
+    },
+    like: function (img) {
       let indx = this.allCats.indexOf(img);
       this.likeCats.push(this.allCats[indx]);
     },
     disLike: function (img) {
       let indx = this.likeCats.indexOf(img);
       this.likeCats.splice(indx, 1);
+    },
+    checkFav: function (img) {
+      return this.likeCats.includes(img) || img === this.hoverCat;
+    },
+    hover: function (cat) {
+      this.hoverCat = cat;
+    },
+    unhover: function () {
+      this.hoverCat = null;
     }
   },
   mounted: function () {
@@ -28,7 +42,6 @@ new Vue({
       }
     })
       .then((response) => response.json())
-      .then((data) => (this.allCats = data))
-      .then(console.log(this.allCats));
+      .then((data) => (this.allCats = data));
   }
 });
